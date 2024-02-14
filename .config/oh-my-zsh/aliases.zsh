f98af3a4522e1bd3862bf3e8b3d706c02eb212f4
Noah <3615061-howboutno@users.noreply.gitlab.com>
2024-02-14 17:05:12 -0500

    Updates

---
+++ .config/oh-my-zsh/aliases.zsh
@@ -0,0 +1,13 @@
+# functions
+alias quit="exit"
+alias q="quit"
+
+# ls
+alias ls="lsd"
+alias l="ls -hF"
+alias la="l -A"
+alias ll="l -l"
+alias lla="la -l"
+
+# nvim
+alias vim="nvim"
---
+++ .config/oh-my-zsh/login-info.zsh
@@ -0,0 +1,2 @@
+export PF_INFO="ascii title os host kernel uptime pkgs memory shell editor wm de palette"
+pfetch
